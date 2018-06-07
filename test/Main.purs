db22cd99c7a39a8ab9e2fd0b3765b84b0ad08a88
Jorge Acereda <jacereda@gmail.com>
2018-06-08 01:56:24 +0200

    Use Effect.Class.Console

--- test/Main.purs
+++ test/Main.purs
@@ -7,14 +7,12 @@ import Data.Either (Either(..))
 import Data.Image.JPEG.Decoder (decode)
 import Effect (Effect)
 import Effect.Aff (Aff, attempt, launchAff_)
-import Effect.Class (liftEffect)
-import Effect.Console (log)
+import Effect.Class.Console (log)
 import Network.HTTP.Affjax (get)
 import Network.HTTP.Affjax.Response as Response
 
 main :: Effect Unit
 main = launchAff_ do
-  tst "https://sateuhtoaeuteosua.net/computervision/images/3/34/Lenna.jpg"
   tst "https://vignette2.wikia.nocookie.net/computervision/images/3/34/Lenna.jpg"
   tst "https://vignette2.wikia.nocookie.net/computervision/foo"
   where tst url = do
@@ -24,8 +22,7 @@ main = launchAff_ do
              Right res -> do
                case decode res.response of
                  Left err -> report err
-                 Right im -> print $ show  im.width <> "x" <> show im.height <> " length:" <> show (byteLength im.data)
+                 Right im -> log $ show  im.width <> "x" <> show im.height <> " length:" <> show (byteLength im.data)
                pure unit
           pure unit
-        print = liftEffect <<< log
-        report = print <<< show
+        report = log <<< show
